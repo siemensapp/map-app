79f40cdbc21a9bbc4c1aea75324e0f10ce1f50d7
Diego R <diefrodriguezcha@unal.edu.co>
2019-03-15 16:47:24 -0500

    Inicio geolocalizacion

---
+++ src/assets/js/arcgis-map.js
@@ -0,0 +1,48 @@
+require([
+  "esri/WebMap",
+  "esri/views/MapView"
+], function (
+    WebMap, MapView
+) {
+
+  /************************************************************
+   * Creates a new WebMap instance. A WebMap must reference
+   * a PortalItem ID that represents a WebMap saved to
+   * arcgis.com or an on-premise portal.
+   *
+   * To load a WebMap from an on-premise portal, set the portal
+   * url with esriConfig.portalUrl.
+   ************************************************************/
+    
+  /************************************************************
+   * Set the WebMap instance to the map property in a MapView.
+   ************************************************************/
+  var webmap = new WebMap({
+    basemap: "dark-gray"
+  });
+
+  var view = new MapView({
+    map: webmap,
+    container: "viewDiv",
+    center: [-74.183962, 4.776951],
+    zoom: 16
+  });
+
+  var boton = document.createElement("button");
+  boton.style.height = "32px";
+  boton.style.width = "32px";
+  boton.style.backgroundColor = "#fff";
+  boton.style.border = "none";
+  boton.style.color = "#6e6e6e";
+  boton.innerHTML = '<i class="fas fa-map-marker-alt"></i>';
+  boton.style.fontSize = "20px";
+  boton.onmouseover = function(){
+    this.style.backgroundColor = "lightGrey";
+    boton.style.cursor = 'pointer';
+  }
+  boton.onmouseout = function(){
+    this.style.backgroundColor = "#fff";
+  }
+  view.ui.add(boton, "top-left");
+
+});
