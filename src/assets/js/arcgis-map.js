2cd6ecaacf8da09fa68f0b8ab6860444872e239b
Rodriguez Chaparro <diego.rodriguez_chaparro@siemens.com>
2019-03-20 14:59:26 -0500

    Started Mysql tests

--- src/assets/js/arcgis-map.js
+++ src/assets/js/arcgis-map.js
@@ -1,166 +1,168 @@
 require([
-    "esri/WebMap",
-    "esri/views/MapView",
-    "esri/geometry/Point",
-    "esri/tasks/Locator",
-    "esri/Graphic"
+  "esri/WebMap",
+  "esri/views/MapView",
+  "esri/geometry/Point",
+  "esri/tasks/Locator",
+  "esri/Graphic"
 ], function (
-    WebMap, MapView, Point, Locator, Graphic
+  WebMap, MapView, Point, Locator, Graphic
 ) {
-    var workers = {};
-    var clicked = false;
-    var tracking;
+  var workers = {};
+  var clicked = false;
+  var tracking;
 
-    var webmap = new WebMap({
-        basemap: "topo"
+  var webmap = new WebMap({
+    basemap: "topo"
+  });
+
+  var view = new MapView({
+    map: webmap,
+    container: "viewDiv",
+    center: [-74.183962, 4.776951],
+    zoom: 16
+  });
+
+  // Se crea el boton para utilizar el API de Geolocalizacion de HTML5
+  var boton = document.createElement("button");
+  boton.style.height = "32px";
+  boton.style.width = "32px";
+  boton.style.backgroundColor = "#fff";
+  boton.style.border = "none";
+  boton.style.color = "#6e6e6e";
+  boton.innerHTML = '<i class="fas fa-map-marker-alt"></i>';
+  boton.style.fontSize = "20px";
+  boton.onmouseover = function () {
+    this.style.backgroundColor = "lightGrey";
+    boton.style.cursor = 'pointer';
+  }
+  boton.onmouseout = function () {
+    this.style.backgroundColor = "#fff";
+  }
+
+  // Seccion de geolocalizacion
+
+  boton.onclick = function () {
+    if (!clicked) {
+      startTracking();
+    } else {
+      stopTracking();
+    }
+  };
+
+  function getRecords() {
+    const Http = new XMLHttpRequest();
+    const url = 'http://localhost:3000/api/workers';
+    Http.open("GET", url);
+    Http.send();
+    Http.onreadystatechange = (e) => {
+      console.log(Http.responseText)
+    }
+  };
+
+  function stopTracking() {
+    clearTimeout(tracking);
+    clicked = false;
+  };
+
+  function startTracking() {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    clicked = true;
+    tracking = setTimeout(startTracking, 30000);
+  };
+
+  function showPosition(position) {
+    if (position !== undefined) {
+      console.log("Latitude: " + position.coords.latitude +
+        "\nLongitude: " + position.coords.longitude);
+      showOwnLocation(position, "John Milton");
+    }
+  }
+
+  // Pone el punto en su lugar del mapa
+
+  function showOwnLocation(position, worker) {
+    getRecords();
+    var pointMap, markerSymbol, pointGraphic;
+    var address = {};
+    view.popup.close();
+    view.graphics.removeAll();
+
+    workers[worker] = new Point({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+    console.log(workers);
+
+    var locator = new Locator({
+      url: "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"
     });
 
-    var view = new MapView({
-        map: webmap,
-        container: "viewDiv",
-        center: [-74.183962, 4.776951],
-        zoom: 16
-    });
+    // Servicio de Geocoder de Arcgis
 
-    // Se crea el boton para utilizar el API de Geolocalizacion de HTML5
+    locator.locationToAddress(workers[worker]).then(function (response) {
+      address = response.attributes;
+      console.log(address);
+      pointMap = {
+        type: "point",
+        longitude: workers[worker].longitude,
+        latitude: workers[worker].latitude
+      };
 
-    var boton = document.createElement("button");
-    boton.style.height = "32px";
-    boton.style.width = "32px";
-    boton.style.backgroundColor = "#fff";
-    boton.style.border = "none";
-    boton.style.color = "#6e6e6e";
-    boton.innerHTML = '<i class="fas fa-map-marker-alt"></i>';
-    boton.style.fontSize = "20px";
-    boton.onmouseover = function () {
-        this.style.backgroundColor = "lightGrey";
-        boton.style.cursor = 'pointer';
-    }
-    boton.onmouseout = function () {
-        this.style.backgroundColor = "#fff";
-    }
-
-    // Seccion de geolocalizacion
-
-    boton.onclick = function () {
-        if (!clicked){
-            startTracking();
-        } else {            
-            stopTracking();
+      markerSymbol = {
+        type: "simple-marker",
+        color: [226, 119, 40],
+        size: "25px",
+        outline: {
+          color: [255, 255, 255],
+          width: 2
         }
-        
-    };
+      };
 
-    function stopTracking() {
-        clearTimeout(tracking);
-        clicked = false;
-    };
-
-    function startTracking() {
-        navigator.geolocation.getCurrentPosition(showPosition);
-        clicked = true;
-        tracking = setTimeout(startTracking, 30000);
-    };
-
-    function showPosition(position) {
-        if (position !== undefined) {
-            console.log("Latitude: " + position.coords.latitude +
-                "\nLongitude: " + position.coords.longitude);
-            showOwnLocation(position, "John Milton");
+      pointGraphic = new Graphic({
+        geometry: pointMap,
+        symbol: markerSymbol,
+        popupTemplate: {
+          title: "John Milton",
+          content: "Servicio de Field Service<br>[ " + workers[worker].longitude + ", " + workers[worker].latitude + " ]<br>" + address.CountryCode + ", " + address.City + ", " + address.PlaceName,
         }
-    }
+      });
 
-    // Pone el punto en su lugar del mapa
+      view.graphics.add(pointGraphic);
 
-    function showOwnLocation(position, worker) {
-        var pointMap, markerSymbol, pointGraphic;
-        var address={};
-        view.popup.close();
-        view.graphics.removeAll();
-        
-        workers[worker] = new Point({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-        });
-        
-        // var currentPosition = new Point({
-        //     latitude: position.coords.latitude,
-        //     longitude: position.coords.longitude
-        // });
+      //showInfo(workers[worker], address);
+    }, function (err) {
+      console.log('Error on locator element.');
+    })
+  }
 
-        // console.log(currentPosition);
-        console.log(workers);
+  // function showInfo(position, address) {
+  //     latitud=position.latitude;
+  //     longitud=position.longitude;
+  //     direccion=address;
+  //     view.popup.open({
+  //         title: "John Milton",
+  //         content: "Servicio de Field Service<br>[ " + longitud + ", " + latitud + " ]<br>" + address.CountryCode + ", " + address.City + ", " + address.PlaceName,            
+  //         location: currentPosition
+  //     })
+  // };
 
-        var locator = new Locator({
-            url: "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"
-        });
+  view.ui.add(boton, "top-left");
 
-        // Servicio de Geocoder de Arcgis
+  // view.on("click", function(event) {
 
-        locator.locationToAddress(workers[worker]).then(function (response) {
-            address = response.attributes;
-            console.log(address);
-            pointMap = {
-                type: "point",
-                longitude: workers[worker].longitude,
-                latitude: workers[worker].latitude
-            };
-    
-            markerSymbol = {
-                type: "simple-marker",
-                color: [226, 119, 40],
-                size: "25px",
-                outline: {
-                    color: [255, 255, 255],
-                    width: 2
-                }
-            };
-    
-            pointGraphic = new Graphic({
-                geometry: pointMap,
-                symbol: markerSymbol,
-                popupTemplate: {
-                    title : "John Milton",
-                    content: "Servicio de Field Service<br>[ " + workers[worker].longitude + ", " + workers[worker].latitude + " ]<br>" + address.CountryCode + ", " + address.City + ", " + address.PlaceName,            
-                }
-            });
-
-            view.graphics.add(pointGraphic);
-
-            //showInfo(workers[worker], address);
-        }, function (err) {
-            console.log('Error on locator element.');
-        })
-    }
-
-    // function showInfo(position, address) {
-    //     latitud=position.latitude;
-    //     longitud=position.longitude;
-    //     direccion=address;
-    //     view.popup.open({
-    //         title: "John Milton",
-    //         content: "Servicio de Field Service<br>[ " + longitud + ", " + latitud + " ]<br>" + address.CountryCode + ", " + address.City + ", " + address.PlaceName,            
-    //         location: currentPosition
-    //     })
-    // };
-
-    view.ui.add(boton, "top-left");
-    
-    // view.on("click", function(event) {
-
-    //     // Get the coordinates of the click on the view
-    //     var lat = Math.round(event.mapPoint.latitude * 1000) / 1000;
-    //     var la = lat.toFixed(3);
-    //     var lon = Math.round(event.mapPoint.longitude * 1000) / 1000;
-    //     var lo = lon.toFixed(3);
-    //     if(latitud.toFixed(3) == la && longitud.toFixed(3) == lo){
-    //     view.popup.open({
-    //       // Set the popup's title to the coordinates of the location
-    //       title: "John Milton",  
-    //       content: "Servicio de Filed Service<br>"+ address.CountryCode + ", " + address.City +", "+address.PlaceName,        
-    //       location: event.mapPoint // Set the location of the popup to the clicked location
-    //     });
-    //     }
-    // });
+  //     // Get the coordinates of the click on the view
+  //     var lat = Math.round(event.mapPoint.latitude * 1000) / 1000;
+  //     var la = lat.toFixed(3);
+  //     var lon = Math.round(event.mapPoint.longitude * 1000) / 1000;
+  //     var lo = lon.toFixed(3);
+  //     if(latitud.toFixed(3) == la && longitud.toFixed(3) == lo){
+  //     view.popup.open({
+  //       // Set the popup's title to the coordinates of the location
+  //       title: "John Milton",  
+  //       content: "Servicio de Filed Service<br>"+ address.CountryCode + ", " + address.City +", "+address.PlaceName,        
+  //       location: event.mapPoint // Set the location of the popup to the clicked location
+  //     });
+  //     }
+  // });
 
 });
